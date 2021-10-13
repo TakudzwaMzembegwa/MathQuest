ac265d72da5cf6b61af36201e78fcb5b3a8d5ff7
TeeKay <takudzwamzembegwa@gmail.com>
2021-10-13 12:50:49 +0200

    Add project files.

---
+++ MathQuest/MathQuest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MathQuest.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MathQuest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MathQuest/MathQuest/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MathQuest.Models;
+using MathQuest.ViewModels;
+using MathQuest.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MathQuest.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
