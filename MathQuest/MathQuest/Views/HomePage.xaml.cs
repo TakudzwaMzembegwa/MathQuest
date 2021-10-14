9beeff3b46b2ee302243ea61cb3ff7a1ec2bee9e
TeeKay <takudzwamzembegwa@gmail.com>
2021-10-14 02:52:10 +0200

    Added homepage view

---
+++ MathQuest/MathQuest/Views/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MathQuest.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public string difficultyLevel;
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        /*
+       * RadioButton CheckChange Event Handler
+       */
+        void RadioSelected(object sender, CheckedChangedEventArgs e)
+        {
+            difficultyLevel = (sender as RadioButton).Content.ToString();
+        }
+
+        public void BtnSelect_Case(object sender, EventArgs e)
+        { 
+        }
+    }
+}
