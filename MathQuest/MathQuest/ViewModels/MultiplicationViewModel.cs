143ed032a247dc792cd5aa280a3572327cd77791
TeeKay <takudzwamzembegwa@gmail.com>
2021-10-16 16:09:38 +0200

    added solutions to bindings

--- MathQuest/MathQuest/ViewModels/MultiplicationViewModel.cs
+++ MathQuest/MathQuest/ViewModels/MultiplicationViewModel.cs
@@ -10,8 +10,14 @@ namespace MathQuest.ViewModels
 {
     public class MultiplicationViewModel : INotifyPropertyChanged
     {
-        const int counter = 10;
-        public static int max = 0;
+        public static int max = 5;
+        public int totalCorrect = 0;
+        const int maxRuns = 10;
+        int counter = 0;
+        string feedback = "";
+        string feedbackColor = "";
+        string symbol = "";
+        string currentProgress = "0";
         string entry = "0";
         string num1 = "0";
         string num2 = "0";
@@ -23,10 +29,30 @@ namespace MathQuest.ViewModels
         {
             Num1 = rand.Next(max + 1).ToString();
             Num2 = rand.Next(max + 1).ToString();
+            Feedback = "MathQuest";
+            FeedbackColor = "#24a0ed";
+            Symbol = "⏭";
+            CurrentProgress = "0.1";
 
-            ClearCommand = new Command(
+            CheckCommand = new Command(
                 execute: () =>
                 {
+                
+                    if (!feedback.Equals("MathQuest"))
+                    {
+                        return;
+                    }
+                    else if (entry.Equals((int.Parse(num1) * int.Parse(num2)).ToString())) {
+                        Feedback = "Correct";
+                        FeedbackColor = "#0F0";
+                        totalCorrect++;
+                    }
+                    else
+                    {
+                        Feedback = "Wrong\nAnswer is: " + (int.Parse(num1) * int.Parse(num2)).ToString();
+                        FeedbackColor = "#FF0000";
+                    }
+                    counter++;
                     Entry = "0";
                     RefreshCanExecutes();
                 });
@@ -62,9 +88,22 @@ namespace MathQuest.ViewModels
 
             NextCommand = new Command(execute: () =>
             {
+                counter++;
+                if (counter == 9)
+                {
+                    Symbol = "⟲";
+                }
+                else if (counter > 9)
+                {
+                    counter = 0;
+                    Symbol = "⏭";
+                }
                 Num1 = rand.Next(max + 1).ToString();
                 Num2 = rand.Next(max + 1).ToString();
-
+                Feedback = "MathQuest";
+                FeedbackColor = "#24a0ed";
+                CurrentProgress = ((counter + 1) / (maxRuns * 1.0)).ToString();
+                Entry = "0";
                 RefreshCanExecutes();
             });
         }
@@ -81,6 +120,67 @@ namespace MathQuest.ViewModels
             get
             {
                 return entry;
+            }
+        }
+
+        public string CurrentProgress
+        {
+            private set
+            {
+                if (currentProgress != value)
+                {
+                    currentProgress = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentProgress"));
+                }
+            }
+            get
+            {
+                return currentProgress;
+            }
+        }
+        public string Feedback
+        {
+            private set
+            {
+                if (feedback != value)
+                {
+                    feedback = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Feedback"));
+                }
+            }
+            get
+            {
+                return feedback;
+            }
+        }
+        public string FeedbackColor
+        {
+            private set
+            {
+                if (feedbackColor != value)
+                {
+                    feedbackColor = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FeedbackColor"));
+                }
+            }
+            get
+            {
+                return feedbackColor;
+            }
+        }
+        public string Symbol
+        {
+            private set
+            {
+                if (symbol != value)
+                {
+                    symbol = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Symbol"));
+                }
+            }
+            get
+            {
+                return symbol;
             }
         }
 
@@ -116,7 +216,7 @@ namespace MathQuest.ViewModels
             }
         }
 
-        public ICommand ClearCommand { private set; get; }
+        public Command CheckCommand { get; private set; }
 
         public ICommand BackspaceCommand { private set; get; }
 
